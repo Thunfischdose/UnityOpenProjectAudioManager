65463b8a157815e6d82e12bbc567e763bb2932b6
Thunfischdose <t.rosen@web.de>
2022-01-16 12:16:36 +0100

    initial commit

---
+++ Runtime/Scripts/SceneManagement/ScriptableObjects/GameSceneSO.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// This class is a base class which contains what is common to all game scenes (Locations, Menus, Managers)
+/// </summary>
+public class GameSceneSO : DescriptionBaseSO
+{
+	public AudioCueSO musicTrack;
+	
+}
